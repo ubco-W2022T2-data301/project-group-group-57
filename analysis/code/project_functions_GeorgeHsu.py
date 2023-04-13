240308efaf51ec9c0e2dfd2b2dff89a8f3ca39c9
George Hsu <hhps507@student.ubc.ca>
2023-04-13 14:50:22 -0700

    Done

--- analysis/code/project_functions_GeorgeHsu.py
+++ analysis/code/project_functions_GeorgeHsu.py
@@ -8,9 +8,9 @@ def load_and_process():
     
     df = (
         pd.read_csv('/Users/georgehsu/project-group-group-57/data/raw/MutualFunds.csv')
-        .assign(Standard_Deviation=lambda x: x.iloc[:, 56:67].std(axis=1, numeric_only=True),
-                Mean=lambda x: x.iloc[:, 56:67].mean(axis=1, numeric_only=True),
-                fund_sector_differentiation=lambda x: np.where(x['Mean'] < 0.29697 , 'Low', 
+        .assign(Standard_Deviation=lambda x: x.iloc[:, 50:150].std(axis=1, numeric_only=True),
+                Mean=lambda x: x.iloc[:, 50:150].mean(axis=1, numeric_only=True),
+                fund_sector_differentiation=lambda x: np.where(x['Mean'] < 0.029697 , 'Low', 
                                                                np.where(x['Mean'] > 0.10640, 'High', 
                                                                         'Moderate')))
         .loc[:, ['fund_symbol', 'fund_return_2021_q2', 'fund_return_2021_q1', 'fund_return_2020_q4', 
